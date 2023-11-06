aaef8aa76c1fa809bb71b36bf6eb8628bad2f6c3
Muhammad Ohee <md.ohee786@gmail.com>
2023-11-06 07:37:33 +0600

    version 1

--- module7.2.php
+++ module7.2.php
@@ -1,5 +1,47 @@
 <?php
+    # Write your PHP code from here
+    fscanf(STDIN, "%s %s", $str1, $str2);
 
+    $len = strlen($str1);
 
+    function stringRotation($index, $len, $str1){
+        
+        $sub1 = "";
+        $sub2 = "";
 
+        $new_len = $len - $index;
+
+        $first_index = $index;
+
+        for($i=0; $i<$new_len; $i++){
+            if(!empty($str1[$index])){
+                $sub1 = $sub1 . $str1[$index];
+                $index += 1;
+            }
+        }
+
+        for($i=0; $i<$first_index; $i++){
+            $sub2 = $sub2 . $str1[$i];    
+        }
+
+        return $sub1 . $sub2;
+    }
+
+    $flag = false;
+
+    for($i=0; $i<$len; $i++){
+        $rotaredString = "";
+        if($str2[0] == $str1[$i]){
+            $rotaredString = stringRotation($i, $len, $str1);
+        }
+        if($rotaredString == $str2){
+            $flag = true;
+        }
+    }
+
+    if($flag){
+        echo "True";
+    } else{
+        echo "False";
+    }
 ?>
